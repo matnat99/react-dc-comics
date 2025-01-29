43530a0f63b4d084ad8bd0945fa1c51ccb5aa79f
Mattia Natale <mattianatale07@gmail.com>
2025-01-29 18:54:48 +0100

    feat: add footer-top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,38 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="footer-top">
+        <div className="container">
+          <ul>
+            <li>
+              <img
+                src="/img/buy-comics-digital-comics.png"
+                alt="Digital Comics"
+              />
+              <a href="#">DIGITAL COMICS</a>
+            </li>
+            <li>
+              <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
+              <a href="#">DC MERCHANDISE</a>
+            </li>
+            <li>
+              <img src="/img/buy-comics-subscriptions.png" alt="Subscription" />
+              <a href="#">SUBSCRIPTION</a>
+            </li>
+            <li>
+              <img
+                src="/img/buy-comics-shop-locator.png"
+                alt="Comic Shop Locator"
+              />
+              <a href="#">COMIC SHOP LOCATOR</a>
+            </li>
+            <li>
+              <img src="/img/buy-dc-power-visa.svg" alt="Dc Power Visa" />
+              <a href="#">DC POWER VISA</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
