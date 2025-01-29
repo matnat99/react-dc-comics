e116c897ffe6cdb99bf16e6b194b35bca84152f3
Mattia Natale <mattianatale07@gmail.com>
2025-01-29 22:34:05 +0100

    feat: add FooterTop component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,9 @@
+import FooterTop from "./FooterTop";
+
 export default function Footer() {
   return (
     <footer>
-      {/* Footer Top */}
-      <div className="footer-top">
-        <div className="container">
-          <ul>
-            <li>
-              <img
-                src="/img/buy-comics-digital-comics.png"
-                alt="Digital Comics"
-              />
-              <a href="#">DIGITAL COMICS</a>
-            </li>
-            <li>
-              <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
-              <a href="#">DC MERCHANDISE</a>
-            </li>
-            <li>
-              <img src="/img/buy-comics-subscriptions.png" alt="Subscription" />
-              <a href="#">SUBSCRIPTION</a>
-            </li>
-            <li>
-              <img
-                src="/img/buy-comics-shop-locator.png"
-                alt="Comic Shop Locator"
-              />
-              <a href="#">COMIC SHOP LOCATOR</a>
-            </li>
-            <li>
-              <img src="/img/buy-dc-power-visa.svg" alt="Dc Power Visa" />
-              <a href="#">DC POWER VISA</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <FooterTop />
       {/* Footer Center */}
       <div className="footer-center">
         <div className="container">
