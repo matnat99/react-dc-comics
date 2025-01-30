68551e0704b4b76dedce5c00f0ad88ad9c98da13
Mattia Natale <mattianatale07@gmail.com>
2025-01-30 15:26:50 +0100

    feat: add footer components

---
+++ src/components/FooterBottom.jsx
@@ -0,0 +1,43 @@
+export default function FooterBottom() {
+  return (
+    <div className="footer-bottom">
+      <div className="container">
+        <div className="footer-bottom-flex">
+          <div>
+            <button>SIGN-UP NOW!</button>
+          </div>
+          <div className="follow-us">
+            <h3>FOLLOW US</h3>
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="/img/footer-facebook.png" alt="Facebook" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="/img/footer-twitter.png" alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="/img/footer-youtube.png" alt="You Tube" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="/img/footer-pinterest.png" alt="Pinterest" />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="/img/footer-periscope.png" alt="Periscope" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
